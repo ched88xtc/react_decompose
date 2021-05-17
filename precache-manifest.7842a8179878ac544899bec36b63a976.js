4df96fbd22794154aa01303338960b58d55ac75e
ched88xtc <ched88official@gmail.com>
2021-05-17 16:35:57 +0300

    Updates

---
+++ precache-manifest.7842a8179878ac544899bec36b63a976.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93bea89a3d2886a61ace5d3c2586c7b9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "34d4d06441fac38a5443",
+    "url": "./static/css/main.8c3340d1.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34d4d06441fac38a5443",
+    "url": "./static/js/main.25c9e03b.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
